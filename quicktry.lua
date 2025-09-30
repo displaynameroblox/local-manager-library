efe55cb3a9e3bffb07f50f5a664182b3924059c1
displaynameroblox <a14111alomari@gmail.com>
2025-09-30 23:17:15 +0300

    Create quicktry.lua

---
+++ quicktry.lua
@@ -0,0 +1,21 @@
+-- here you can use local manager for a lot of things, like a media player.
+
+local manager = loadstring(game:HttpGet("https://raw.githubusercontent.com/displaynameroblox/localmaner/main/localmaner.lua"))() -- load the library
+
+local settings = {} -- for settings,
+
+local function settingshandler(act)
+    local avalibeleacts = {
+        "removedata",
+        "add",
+        "load",
+        "new"
+    }
+    if act == new then
+        local newsettings = {
+            playlist = {},
+            sounds = {}
+        }
+        if isfile("")
+    end
+end
