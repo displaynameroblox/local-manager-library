880865991320881cb1f70168a9890cc9240ab01c
displaynameroblox <a14111alomari@gmail.com>
2025-10-04 14:24:54 +0300

    :3

---
+++ scripts/lib/NEWlocalmanager.lua
@@ -0,0 +1,21 @@
+-- well well well, a rewrite of the whole fucking library. that will surly will go well right???
+
+local manager = {}
+
+local _debug
+
+function _debugtoggle(_Debug)
+    if _Debug == true then
+        _debug = true
+    elseif _Debug == false then
+        _debug = false
+    else
+        return "invaild act"
+    end
+end
+
+function Media(islocal, isroblox, type, media)
+
+end
+
+return manager
